1f2eeb56f887f0c2e06c0a0f35d02f575c084e4e
claudealdric <cvaldric@gmail.com>
2024-07-21 18:48:04 -0400

    Implement the exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,7 +11,17 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		input = strings.Replace(input, "\n", "", 1)
-		fmt.Printf("%s: command not found\n", input)
+		command, _ := ParseInput(input)
+		if command == "exit" {
+			os.Exit(0)
+		}
+
+		fmt.Printf("%s: command not found\n", command)
 	}
+}
+
+func ParseInput(input string) (command string, args []string) {
+	input = strings.Replace(input, "\n", "", 1)
+	parts := strings.Split(input, " ")
+	return parts[0], parts[1:]
 }
