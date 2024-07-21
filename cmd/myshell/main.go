98745e694e80943e476012cf42be06895e37ed75
claudealdric <cvaldric@gmail.com>
2024-07-21 18:13:46 -0400

    Implement support for handling missing commands in the shell

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	input = strings.Replace(input, "\n", "", 1)
+	fmt.Printf("%s: command not found\n", input)
 }
