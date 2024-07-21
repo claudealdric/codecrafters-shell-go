eb8957e2870cf0307222f3a5871152f941ade23f
claudealdric <cvaldric@gmail.com>
2024-07-21 18:25:11 -0400

    Implement REPL (Read-Eval-Print Loop)

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	input = strings.Replace(input, "\n", "", 1)
-	fmt.Printf("%s: command not found\n", input)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.Replace(input, "\n", "", 1)
+		fmt.Printf("%s: command not found\n", input)
+	}
 }
